374c86772177d1e96a9cfe8e56d77d625b487b51
Paul <thepaulxxx@gmail.com>
2012-02-22 14:27:20 +0600

    update config. but still error with copybit

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(my-dir)
 
 ifeq ($(TARGET_DEVICE),mimmi)
     subdir_makefiles := \
-        $(LOCAL_PATH)/libcopybit/Android.mk \
         $(LOCAL_PATH)/hciattach/Android.mk \
         $(LOCAL_PATH)/libaudio/Android.mk \
         $(LOCAL_PATH)/libstagefrighthw/Android.mk
